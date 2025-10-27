484a22b5298940a4510b49d5fcfb920e8177a087
Rim Faritov <faritov.rome@gmail.com>
2025-10-27 12:20:43 +0500

    add main.py with 2 classes and tests for main.py

---
+++ main.py
@@ -0,0 +1,24 @@
+class Product:
+    """Класс для создания наименований продуктов и их параметров"""
+
+    def __init__(self, name, description, price, quantity):
+        self.name = name
+        self.description = description
+        self.price = price
+        self.quantity = quantity
+
+
+class Category:
+    """Класс для создания категорий продукции"""
+
+    category_count = 0
+    product_count = 0
+
+    def __init__(self, name, description, products):
+        self.name = name
+        self.description = description
+        self.products = products
+
+        Category.category_count += 1
+
+        Category.product_count += len(products)
