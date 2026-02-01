d0acf5800d83684a78d3f8fbdbfb6593a5b5662e
Rim Faritov <faritov.rome@gmail.com>
2026-02-01 18:32:04 +0500

    добавлены подклассы для Product: Smartphone, Os, ConnectionSpeed, LawnGrass, GrowthHeight, Resistance.для Smartphone и LawnGrass добавлен метод __add__, проверяющий сложение только объектов одного класса

--- tests/test_category.py
+++ tests/test_category.py
@@ -21,6 +21,7 @@ def test_category_initialization() -> None:
 def test_counts_increase() -> None:
     """Проверяем увеличение чисел в счетчиках"""
     initial_category_count = Category.category_count
+    initial_product_count = Category.product_count
 
     product1 = Product("Samsung Galaxy S23 Ultra", "256GB, Серый цвет, 200MP камера", 180000.0, 5)
     product2 = Product("Iphone 15", "512GB, Gray space", 210000.0, 8)
@@ -31,7 +32,7 @@ def test_counts_increase() -> None:
     "Проверяем, что счетчики увеличились"
     assert Category.category_count == initial_category_count + 1
 
-    assert Category.product_count == initial_product_count + 1
+    assert Category.product_count == initial_product_count + 3
 
 def test_str_category(capsys):
     """Создаем несколько продуктов"""
@@ -48,7 +49,7 @@ def test_str_category(capsys):
     assert captured.out == "Категория, количество продуктов: 5 шт.\n"
 
 
-def test_add_product() -> None:
+def test_add_product_positive() -> None:
 
     product1 = Product("Samsung Galaxy S23 Ultra", "256GB, Серый цвет, 200MP камера", 180000.0, 5)
     product2 = Product("Iphone 15", "512GB, Gray space", 210000.0, 8)
@@ -83,5 +84,18 @@ def test_add_product() -> None:
         assert False, "Ожидалось исключение при добавлении непонятного объекта"
     except TypeError:
         pass  # тест прошел успешно
+
+def test_add_product_negative() -> None:
+    """Проверка вывода ошибки при добавлении обьекта, не входящего в Product"""
+    product1 = Product("Samsung Galaxy S23 Ultra", "256GB, Серый цвет, 200MP камера", 180000.0, 5)
+    product2 = Product("Iphone 15", "512GB, Gray space", 210000.0, 8)
+    product3 = Product("Xiaomi Redmi Note 11", "1024GB, Синий", 31000.0, 14)
+
+    # Создаем категорию
+    category = Category("Электроника", "Технические устройства", [product1, product2, product3])
+
+    # Проверка, что исключение выбрасывается при неправильном типе
+    with pytest.raises(TypeError, match="Объект Не товар недопустим.Можно добавлять только объекты класса Product."):
+        category.add_product("Не товар")
 
 
